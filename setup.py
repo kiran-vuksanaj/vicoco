e78795210e8ecf7e5924f5578994f38109b31c57
Kiran Vuksanaj <kiranv@mit.edu>
2025-10-05 15:12:16 -0400

    require old cocotb for now

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     author_email='kiranv@mit.edu',
     package_dir={"":"src"},
     packages=find_packages(where="./src"),
-    install_requires=['cocotb']
+    install_requires=['cocotb==1.9.2']
 )
